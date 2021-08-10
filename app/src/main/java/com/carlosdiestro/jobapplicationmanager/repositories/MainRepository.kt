2e0adaefe2a82f516e8dafac3c8be5e7812bb497
Carlos Diestro <48551464+K4rLiToX@users.noreply.github.com>
2021-08-10 15:26:22 +0200

    feat: viewmodel and repository set up (#6)

---
+++ app/src/main/java/com/carlosdiestro/jobapplicationmanager/repositories/MainRepository.kt
@@ -0,0 +1,31 @@
+package com.carlosdiestro.jobapplicationmanager.repositories
+
+import com.carlosdiestro.jobapplicationmanager.datasource.dao.JobApplicationDAO
+import com.carlosdiestro.jobapplicationmanager.datasource.entities.JobApplication
+import javax.inject.Inject
+
+class MainRepository @Inject constructor(
+    private val jobApplicationDAO: JobApplicationDAO
+) {
+
+    suspend fun insertJobApplication(jobApplication: JobApplication) =
+        jobApplicationDAO.insertJobApplication(jobApplication)
+
+    suspend fun updateJobApplication(jobApplication: JobApplication) =
+        jobApplicationDAO.updateJobApplication(jobApplication)
+
+    suspend fun clearNonPendingJobApplications() =
+        jobApplicationDAO.clearNonPendingJobApplications()
+
+    fun getAll() =
+        jobApplicationDAO.getAll()
+
+    fun getPendingJobApplications() =
+        jobApplicationDAO.getPendingJobApplications()
+
+    fun getAcceptedJobApplications() =
+        jobApplicationDAO.getAcceptedJobApplications()
+
+    fun getRejectedJobApplications() =
+        jobApplicationDAO.getRejectedJobApplications()
+}
--- app/src/main/java/com/carlosdiestro/jobapplicationmanager/ui/fragments/JobApplicationsFragment.kt
+++ app/src/main/java/com/carlosdiestro/jobapplicationmanager/ui/fragments/JobApplicationsFragment.kt
@@ -1,13 +1,19 @@
 package com.carlosdiestro.jobapplicationmanager.ui.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import com.carlosdiestro.jobapplicationmanager.R
+import com.carlosdiestro.jobapplicationmanager.ui.viewmodels.MainViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class JobApplicationsFragment : Fragment() {
+
+    private val viewModel: MainViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
