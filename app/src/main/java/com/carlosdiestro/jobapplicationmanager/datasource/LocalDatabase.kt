a7875004817800f073afb0a184541a111dd9019d
K4rLiToX <carlostroller99@gmail.com>
2021-08-10 12:57:50 +0200

    feat: create local database

---
+++ app/src/main/java/com/carlosdiestro/jobapplicationmanager/datasource/LocalDatabase.kt
@@ -0,0 +1,16 @@
+package com.carlosdiestro.jobapplicationmanager.datasource
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.carlosdiestro.jobapplicationmanager.datasource.dao.JobApplicationDAO
+import com.carlosdiestro.jobapplicationmanager.datasource.entities.JobApplication
+
+@Database(
+    entities = [JobApplication::class],
+    version = 1,
+    exportSchema = false
+)
+abstract class LocalDatabase : RoomDatabase() {
+
+    abstract fun getJobApplicationDAO(): JobApplicationDAO
+}
