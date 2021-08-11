1ed8d30bebd60e302abdac25aec54af5879ed4e3
Carlos Diestro <48551464+K4rLiToX@users.noreply.github.com>
2021-08-11 09:54:22 +0200

    feat: show job applications (#8)

--- app/src/main/java/com/carlosdiestro/jobapplicationmanager/ui/viewmodels/MainViewModel.kt
+++ app/src/main/java/com/carlosdiestro/jobapplicationmanager/ui/viewmodels/MainViewModel.kt
@@ -1,12 +1,17 @@
 package com.carlosdiestro.jobapplicationmanager.ui.viewmodels
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.carlosdiestro.jobapplicationmanager.datasource.entities.JobApplication
 import com.carlosdiestro.jobapplicationmanager.repositories.MainRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class MainViewModel @Inject constructor(
-    private val mainRepository: MainRepository
+    mainRepository: MainRepository
 ) : ViewModel() {
+
+    val jobApplications = mainRepository.getAll()
 }
