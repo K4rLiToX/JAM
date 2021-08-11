a6fa06b2e0f142e01edc744aff7b18532f1bdac1
K4rLiToX <carlostroller99@gmail.com>
2021-08-11 09:51:56 +0200

    feat: design and modelize recyclerview item

--- app/src/main/java/com/carlosdiestro/jobapplicationmanager/datasource/entities/JobApplication.kt
+++ app/src/main/java/com/carlosdiestro/jobapplicationmanager/datasource/entities/JobApplication.kt
@@ -1,7 +1,9 @@
 package com.carlosdiestro.jobapplicationmanager.datasource.entities
 
+import android.content.Context
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import com.carlosdiestro.jobapplicationmanager.R
 
 @Entity(tableName = "application_table")
 data class JobApplication(
@@ -13,4 +15,10 @@ data class JobApplication(
 ) {
     @PrimaryKey(autoGenerate = true)
     var id: Int? = null
+
+    fun mapStatusToString(ctx: Context, status: Int) = when(status) {
+        0 -> ctx.getString(R.string.pending_status)
+        1 -> ctx.getString(R.string.accepted_status)
+        else -> ctx.getString(R.string.rejected_status)
+    }
 }
---
+++ app/src/main/java/com/carlosdiestro/jobapplicationmanager/ui/adapters/JobApplicationAdapter.kt
@@ -0,0 +1,54 @@
+package com.carlosdiestro.jobapplicationmanager.ui.adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.carlosdiestro.jobapplicationmanager.R
+import com.carlosdiestro.jobapplicationmanager.databinding.JobApplicationItemLayoutBinding
+import com.carlosdiestro.jobapplicationmanager.datasource.entities.JobApplication
+
+class JobApplicationAdapter(
+    private val ctx: Context
+) :
+    ListAdapter<JobApplication, JobApplicationAdapter.JobApplicationViewHolder>(
+        JobApplicationDiffUtilCallback
+    ) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): JobApplicationViewHolder {
+        return JobApplicationViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.job_application_item_layout, parent, false))
+    }
+
+    override fun onBindViewHolder(holder: JobApplicationViewHolder, position: Int) {
+        holder.bind(getItem(position))
+    }
+
+    inner class JobApplicationViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val binding = JobApplicationItemLayoutBinding.bind(itemView)
+
+        fun bind(jobApplication: JobApplication) = with(binding) {
+            txtApplicationDate.text = jobApplication.applicationDate
+            txtJobPosition.text = jobApplication.jobPosition
+            txtLocation.text = jobApplication.location
+            txtStatus.text = jobApplication.mapStatusToString(ctx, jobApplication.status)
+            txtStatus.setBackgroundColor(getStatusBackgroundColor(jobApplication.status))
+        }
+
+        private fun getStatusBackgroundColor(status: Int) = when(status) {
+            0 -> ctx.getColor(R.color.light_blue)
+            1 -> ctx.getColor(R.color.light_green)
+            else -> ctx.getColor(R.color.light_red)
+        }
+    }
+}
+
+private object JobApplicationDiffUtilCallback : DiffUtil.ItemCallback<JobApplication>() {
+    override fun areItemsTheSame(oldItem: JobApplication, newItem: JobApplication) =
+        oldItem.id == newItem.id
+
+    override fun areContentsTheSame(oldItem: JobApplication, newItem: JobApplication) =
+        oldItem == newItem
+}
