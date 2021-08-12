cc6fc5ddcd62b777f91f769779012b2d1bf926d9
K4rLiToX <carlostroller99@gmail.com>
2021-08-12 13:52:16 +0200

    feat: create gson single access variable

--- app/src/main/java/com/carlosdiestro/jobapplicationmanager/utils/Constants.kt
+++ app/src/main/java/com/carlosdiestro/jobapplicationmanager/utils/Constants.kt
@@ -1,9 +1,12 @@
 package com.carlosdiestro.jobapplicationmanager.utils
 
+import com.google.gson.Gson
+
 object Constants {
 
     const val LOCALDATABASE_NAME = "localdatase"
     const val PENDING_STATUS = 0
     const val ACCEPTED_STATUS = 1
     const val REJECTED_STATUS = 2
+    val GSON = Gson()
 }
