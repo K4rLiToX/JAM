a2a45dcf288bd0c0c7be1fe77d1b707e971c2b4d
K4rLiToX <carlostroller99@gmail.com>
2021-08-16 17:07:33 +0200

    refactor: remove unused imports

--- app/src/androidTest/java/com/carlosdiestro/jobapplicationmanager/datasource/JobApplicationTest.kt
+++ app/src/androidTest/java/com/carlosdiestro/jobapplicationmanager/datasource/JobApplicationTest.kt
@@ -1,8 +1,6 @@
 package com.carlosdiestro.jobapplicationmanager.datasource
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
-import androidx.room.Room
-import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.SmallTest
 import com.carlosdiestro.jobapplicationmanager.datasource.dao.JobApplicationDAO
