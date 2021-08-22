3dcef769be9ee5ced3bf712a75968730ae698cca
K4rLiToX <carlostroller99@gmail.com>
2021-08-22 15:17:34 +0200

    refactor: make function accesible globally

--- app/src/main/java/com/carlosdiestro/jobapplicationmanager/datasource/entities/JobApplication.kt
+++ app/src/main/java/com/carlosdiestro/jobapplicationmanager/datasource/entities/JobApplication.kt
@@ -6,9 +6,6 @@ import androidx.room.PrimaryKey
 import com.carlosdiestro.jobapplicationmanager.R
 import com.carlosdiestro.jobapplicationmanager.utils.Constants.ACCEPTED_STATUS
 import com.carlosdiestro.jobapplicationmanager.utils.Constants.PENDING_STATUS
-import java.text.SimpleDateFormat
-import java.util.*
-import java.util.logging.SimpleFormatter
 
 @Entity(tableName = "application_table")
 data class JobApplication(
@@ -21,11 +18,9 @@ data class JobApplication(
     @PrimaryKey(autoGenerate = true)
     var id: Int? = null
 
-    fun mapStatusToString(ctx: Context, status: Int) = when(status) {
+    fun mapStatusToString(ctx: Context, status: Int) = when (status) {
         PENDING_STATUS -> ctx.getString(R.string.pending_status)
         ACCEPTED_STATUS -> ctx.getString(R.string.accepted_status)
         else -> ctx.getString(R.string.rejected_status)
     }
-
-    fun timeStampToDate(timeStamp: Long) = SimpleDateFormat("dd/MM/yyyy", Locale.US).format(timeStamp)
 }
